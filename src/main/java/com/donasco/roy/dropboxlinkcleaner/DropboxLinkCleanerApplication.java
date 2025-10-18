4fb92c11962d37852055efd31c71f680acb6c829
Roy Donasco <roy.donasco@sevenhatssolutions.com>
2025-10-18 13:02:37 +0800

    Update LICENSE with the correct copyright attribution

--- src/main/java/com/donasco/roy/dropboxlinkcleaner/DropboxLinkCleanerApplication.java
+++ src/main/java/com/donasco/roy/dropboxlinkcleaner/DropboxLinkCleanerApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 The Dropbox Link Cleaner contributors
+ * Copyright 2025 Roy Donasco
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
